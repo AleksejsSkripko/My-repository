d29042e0c2c14975c0e0b0780b94b069c337c12d
Aleksejs Skripko <skripko.alexey@gmail.com>
2021-11-12 00:02:28 +0100

    Create home-work-for-12.11.2021-Classes-and-objects-main-Student.java

---
+++ home-work-for-12.11.2021-Classes-and-objects-main-Student.java
@@ -0,0 +1,32 @@
+package homeWork;
+
+import javax.management.ValueExp;
+
+public class Student {
+
+        // Д/З
+
+        //1.Создать класс    „ Student“  ,
+        // содержащий 2 переменные
+        // String „name“ и  int „number“.
+        // В методе main создать обьект класса Student,
+        // со своим именем и номером 1.
+        //2. Создать еще два обьекта класса студент
+        // с любыми именами и вывести их на экран.
+
+
+        // write your code here
+    public String studentName;
+
+    {
+        studentName = "Alex";
+    }
+
+    public int number =1;
+
+
+
+
+
+    }
+
